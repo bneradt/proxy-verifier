243f31d033d6f284a3d49e4287b9d3c901c7b16d
bneradt <bneradt@verizonmedia.com>
2021-04-13 11:11:23 -0500

    Add validation failure tests.

--- test/autests/gold_tests/http3/http3.test.py
+++ test/autests/gold_tests/http3/http3.test.py
@@ -33,20 +33,34 @@ server.Streams.stdout += Testers.ExcludesExpression(
     "Violation:",
     "There should be no verification errors because there are none added.")
 
-##
-## Test 1: Verify correct verification failure behaviors.
-##
-#r = Test.AddTestRun("Verify HTTP/3 with verification failures")
-#client = r.AddClientProcess("client2", "replay_files/http3_to_http1_failures.yaml")
-#server = r.AddServerProcess("server2", "replay_files/http3_to_http1_failures.yaml")
-#proxy = r.AddProxyProcess("proxy2", listen_port=client.Variables.http3_port,
-#                          server_port=server.Variables.http_port,
-#                          use_ssl=True, use_http3_to_1=True)
 #
-#client.Streams.stdout += Testers.ExcludesExpression(
-#    "Violation:",
-#    "There should be no verification errors because there are none added.")
+# Test 2: Verify correct verification failure behaviors.
 #
-#server.Streams.stdout += Testers.ExcludesExpression(
-#    "Violation:",
-#    "There should be no verification errors because there are none added.")
+r = Test.AddTestRun("Verify HTTP/3 with verification failures")
+client = r.AddClientProcess("client2", "replay_files/http3_to_http1_failures.yaml")
+server = r.AddServerProcess("server2", "replay_files/http3_to_http1_failures.yaml")
+proxy = r.AddProxyProcess("proxy2", listen_port=client.Variables.http3_port,
+                          server_port=server.Variables.http_port,
+                          use_ssl=True, use_http3_to_1=True)
+
+client.ReturnCode = 1
+
+client.Streams.stdout += Testers.ContainsExpression(
+    'Equals Violation: Different. .* Field Name: "x-equal-header", Correct Value: "other_content", Actual Value: "some_content"',
+    "There should be an equal violation for x-equal-header.")
+
+client.Streams.stdout += Testers.ContainsExpression(
+    'Equals Success: .* Field Name: "x-response-header", Value: "response"',
+    "Verification should be happy with the X-Response-Header.")
+
+client.Streams.stdout += Testers.ContainsExpression(
+    'Absence Violation: Present. .* Field Name: "x-added-header", Value: "1"',
+    "There should be an absence violation for X-Added-Header because it will be present.")
+
+client.Streams.stdout += Testers.ContainsExpression(
+    'Presence Violation: Absent. .* Field Name: "x-deleted-header"',
+    "There should be an presence violation for X-Deleted-Header because it will be absent.")
+
+client.Streams.stdout += Testers.ContainsExpression(
+    'HTTP/3 Status Violation: expected 502 got 200',
+    "There should a status violation for an unexpected 502 response.")
