54d615fb9b078dcc622b56be9298682d7800f828
Brian Neradt <brian.neradt@gmail.com>
2024-10-03 20:35:56 +0000

    Revert "debug"
    This reverts commit 941825f0c46362190648c8ea203da4248b97a48f.

--- local/src/core/https.cc
+++ local/src/core/https.cc
@@ -8,7 +8,6 @@
 #include "core/https.h"
 #include "core/ProxyVerifier.h"
 
-#include <chrono>
 #include <dirent.h>
 #include <fcntl.h>
 #include <netdb.h>
@@ -20,13 +19,6 @@
 #include "swoc/bwf_ex.h"
 #include "swoc/bwf_ip.h"
 #include "swoc/bwf_std.h"
-
-
-
-
-#include <iostream>
-
-
 
 using swoc::Errata;
 using swoc::TextView;
@@ -366,7 +358,6 @@ TLSSession::accept()
     errata.note(S_ERROR, R"(Failed SSL_set_fd: {}.)", swoc::bwf::SSLError{});
     return errata;
   }
-  auto before = std::chrono::steady_clock::now();
   int retval = SSL_accept(_ssl);
   while (retval < 0) {
     auto const ssl_error = SSL_get_error(_ssl, retval);
@@ -399,10 +390,6 @@ TLSSession::accept()
     }
     // Poll succeeded.
     retval = SSL_accept(_ssl);
-  }
-  auto delta = std::chrono::duration_cast<milliseconds>(std::chrono::steady_clock::now() - before);
-  if (delta > 100ms) {
-    std::cout << "TLS handshake took " << delta.count() << "ms" << std::endl;
   }
   errata.note(S_DIAG, "Finished accept using TLSSession");
   return errata;
@@ -439,7 +426,6 @@ TLSSession::connect(SSL_CTX *client_context)
         _client_verify_mode);
     SSL_set_verify(_ssl, _client_verify_mode, nullptr /* No verify_callback is passed */);
   }
-  auto before = std::chrono::steady_clock::now();
   int retval = SSL_connect(_ssl);
   while (retval < 0) {
     auto const ssl_error = SSL_get_error(_ssl, retval);
@@ -475,11 +461,6 @@ TLSSession::connect(SSL_CTX *client_context)
     // Poll succeeded.
     retval = SSL_connect(_ssl);
   }
-  auto delta = std::chrono::duration_cast<milliseconds>(std::chrono::steady_clock::now() - before);
-  if (delta > 100ms) {
-    std::cout << "TLS handshake took " << delta.count() << "ms" << std::endl;
-  }
-
 
   auto const verify_result = SSL_get_verify_result(_ssl);
   errata.note(
